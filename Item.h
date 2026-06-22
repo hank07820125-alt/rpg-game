daaf88ad75226446f6f80910c4b11a62aa7498ca
s11459041 <s11459041@gm2.nutn.edu.tw>
2026-06-22 15:11:52 +0800

    Add Item.h

--- Item.h
+++ Item.h
@@ -1,1 +1,31 @@
+#ifndef ITEM_H
+#define ITEM_H
 
+#include <string>
+
+using namespace std;
+
+class Item
+{
+private:
+    string name;
+    string type;
+    int effectValue;
+    int quantity;
+
+public:
+    Item();
+    Item(string n, string t, int e, int q);
+
+    void use();
+    void showInfo();
+    bool isAvailable();
+
+    string getName();
+    string getType();
+    int getEffectValue();
+    int getQuantity();
+    void addQuantity(int q);
+};
+
+#endif
