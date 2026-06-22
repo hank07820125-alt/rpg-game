3ae7c1a45be319e3bada2db1def799f9f872297d
hank07820125-alt <hank07820125@gmail.com>
2026-06-22 15:14:24 +0800

    Add skill system

--- Skill.h
+++ Skill.h
@@ -1,1 +1,26 @@
+#ifndef SKILL_H
+#define SKILL_H
 
+#include <string>
+
+using namespace std;
+
+class Skill
+{
+private:
+    string name;
+    int damage;
+    int mpCost;
+
+public:
+    Skill();
+    Skill(string n, int d, int mp);
+
+    void use();
+    void showInfo();
+    int getDamage();
+    int getMpCost();
+    string getName();
+};
+
+#endif
