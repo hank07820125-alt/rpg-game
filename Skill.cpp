1ed2aa6067adc8364bc62b4f23ba5f4d0931df03
hank07820125-alt <hank07820125@gmail.com>
2026-06-22 15:15:00 +0800

    Add skill system

--- Skill.cpp
+++ Skill.cpp
@@ -1,1 +1,44 @@
+#include "Skill.h"
+#include <iostream>
 
+using namespace std;
+
+Skill::Skill()
+{
+    name = "None";
+    damage = 0;
+    mpCost = 0;
+}
+
+Skill::Skill(string n, int d, int mp)
+{
+    name = n;
+    damage = d;
+    mpCost = mp;
+}
+
+void Skill::use()
+{
+    cout << name << " used!" << endl;
+}
+
+void Skill::showInfo()
+{
+    cout << name << " Damage: " << damage
+         << " MP Cost: " << mpCost << endl;
+}
+
+int Skill::getDamage()
+{
+    return damage;
+}
+
+int Skill::getMpCost()
+{
+    return mpCost;
+}
+
+string Skill::getName()
+{
+    return name;
+}
