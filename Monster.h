cc843c1fc2eae32e0da24cd10712a1f6fcc9b38b
Lucas0119738 <lucas01190309@gmail.com>
2026-06-22 15:13:38 +0800

    Add Monster Class

--- Monster.h
+++ Monster.h
@@ -1,1 +1,31 @@
+#ifndef MONSTER_H
+#define MONSTER_H
 
+#include <string>
+
+using namespace std;
+
+class Monster
+{
+private:
+    string name;
+    int hp;
+    int attackPower;
+    int rewardGold;
+    int rewardExp;
+
+public:
+    Monster();
+    Monster(string n, int h, int atk, int gold, int exp);
+
+    int attack();
+    void takeDamage(int damage);
+    bool isAlive();
+    void showInfo();
+
+    string getName();
+    int getRewardGold();
+    int getRewardExp();
+};
+
+#endif
